f4777f47d880a8ce76c7021faf7ecab7769d2954
D-Curse <dhruvbhatada@gmail.com>
2023-09-09 04:16:39 +0530

    backend

--- CareerGuidance/app/models.py
+++ CareerGuidance/app/models.py
@@ -15,3 +15,31 @@ class Course(models.Model):
 
     def __str__(self):
         return f"{self.field_name} - {self.branch}"
+
+class Details(models.Model):
+    CATEGORY_CHOICES = (
+        ('Pros','pros'),
+        ('Cons','cons'),
+    )  
+    
+    main_head = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='data/media')
+    
+    about_career_head = models.CharField(max_length=100)
+    about_career_para = models.TextField()
+    
+
+    def __str__(self):
+        return self.main_head
+    
+class EduDetails(models.Model):
+    
+    main_head = models.CharField(max_length=100)
+    edu_stream = models.CharField(max_length=100)
+    edu_graduation = models.CharField(max_length=100)
+    edu_after_grad = models.CharField(max_length=100)
+    edu_post_grad = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.main_head
+    
