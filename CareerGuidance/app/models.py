9bf759d285ed1226df1f9306e18db9d728dab0ca
D-Curse <dhruvbhatada@gmail.com>
2023-09-08 16:42:24 +0530

    data-model

--- CareerGuidance/app/models.py
+++ CareerGuidance/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    BRANCH_CHOICES = (
+        ('Science', 'science'),
+        ('Commerce', 'commerce'),
+        ('Arts', 'arts'),
+    )
+    
+    index = models.IntegerField(primary_key=True)
+    field_name = models.CharField(max_length=100)
+    branch = models.CharField(max_length=100, choices=BRANCH_CHOICES)
+
+    def __str__(self):
+        return f"{self.field_name} - {self.branch}"
